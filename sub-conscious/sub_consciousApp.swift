e3237a69ecbdbadd267cd7587f9137af4b16e997
Yi Jia Huang <yijh1705@gmail.com>
2022-07-26 23:40:39 -0400

    Initial Commit

---
+++ sub-conscious/sub_consciousApp.swift
@@ -0,0 +1,17 @@
+//
+//  sub_consciousApp.swift
+//  sub-conscious
+//
+//  Created by HDSB on 2022-07-26.
+//
+
+import SwiftUI
+
+@main
+struct sub_consciousApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
